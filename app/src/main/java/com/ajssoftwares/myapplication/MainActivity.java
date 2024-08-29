b30f19000fdebe0f20fb695da7bfb522121d8f61
pasionatedeveloper <passionatedeveloper98@gmail.com>
2024-08-29 20:35:14 +0500

    sample app

--- app/src/main/java/com/ajssoftwares/myapplication/MainActivity.java
+++ app/src/main/java/com/ajssoftwares/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.ajssoftwares.myapplication;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -10,8 +11,10 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -38,9 +41,7 @@ public class MainActivity extends AppCompatActivity implements
 
 
 
-
-
-        Snackbar.make(this.getCurrentFocus(),"Welcome",Snackbar.LENGTH_LONG).show();
+//        Snackbar.make(this.getCurrentFocus(),"Welcome",Snackbar.LENGTH_LONG).show();
 
 
         //toggle buttton for drawer
@@ -62,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements
 
 
 
-        //navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
 
@@ -83,9 +84,41 @@ public class MainActivity extends AppCompatActivity implements
         else if(id == R.id.favouritesItem){
             getSupportFragmentManager().beginTransaction().add(R.id.fragmentContainer,new FavouriteFragment()).commit();
         }
+
+        else if(id == R.id.logoutNavItem){
+            drawerLayout.closeDrawer(GravityCompat.START);
+            showAlertDialog();
+        }
+
         else{
 
         }
         return true;
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Logout");
+        builder.setMessage("Are you sure you want to log out");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                 SharedPreferences sp = getSharedPreferences("registration", Context.MODE_PRIVATE);
+                 SharedPreferences.Editor editor = sp.edit();
+                 editor.putString("isUserLogin","NotLoggedIn");
+                 editor.apply();
+
+                 Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+                 startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
     }
 }
--- app/src/main/java/com/ajssoftwares/myapplication/RegistrationActivity.java
+++ app/src/main/java/com/ajssoftwares/myapplication/RegistrationActivity.java
@@ -1,6 +1,5 @@
 package com.ajssoftwares.myapplication;
 
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
@@ -10,10 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.material.snackbar.Snackbar;
 
 
 public class RegistrationActivity extends AppCompatActivity {
